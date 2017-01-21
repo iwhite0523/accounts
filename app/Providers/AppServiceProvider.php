d89f6deff822aade4e30dc788e875bfaa0680fdf
bisonjames <whitis01@gmail.com>
2017-01-20 18:38:14 -0600

    Allow Foreign Keys when using SQLite.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\DB;
-use \Illuminate\Database\SQLiteConnection;
+use Illuminate\Database\SQLiteConnection;
 
 class AppServiceProvider extends ServiceProvider
 {
