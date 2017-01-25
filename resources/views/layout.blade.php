a5d9f6611b5ad713a76833ab07adca6c24cf37b8
Bison James <bisonjames@Bisons-MacBook-Pro.local>
2017-01-25 17:34:56 -0600

    Hooray, we have a style sheet!! (flexbox)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,91 +6,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         @yield('head')
+        <link rel="stylesheet" href="/css/app.css">
+        {{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">--}}
+
         <title>Treasury</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        <style>
-            rect.highcharts-background {
-                fill: #DADADA;
-            }
 
-            html, body {
-                background-color: #DADADA;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            h1 {
-              color: indigo;
-            }
-
-            a {
-                color: indigo;
-                text-decoration: none;
-            }
-
-            a:hover {
-                color: goldenrod;
-                text-decoration: none;
-            }
-
-            font:hover {
-                color: navy;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <header role="banner"> <!-- Try to figure out how to make a banner -->
-            <a href="/periods"><img id="flag" src="/img/crown.png" width="81" height="54" class="crown"></a>
+        <header role="banner" data-feature="header" data-type="b" data-header-transition-override=""> <!-- Try to figure out how to make a banner -->
+            <h1 class="golden-title"><a href="/periods"><img id="flag" src="/img/crown.png" width="81" height="54" class="crown"></a>Treasury of the King</h1>
         </header>
+
+        <br><br><br>
         @yield('content')
         @yield('footer')
     </body>
