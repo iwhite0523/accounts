e4cbec3c3b222aa9c158cbaefd6b5c78f2c07eff
whitis01 <whitis01@gmail.com>
2017-02-01 13:49:07 -0600

    merge master with develop (#13)
    * Getting some better CSS control over the accounts. Balance newline still issue.
    
    * Quick fix - form tag out of place

--- resources/views/periods/show.blade.php
+++ resources/views/periods/show.blade.php
@@ -5,48 +5,52 @@
     {!! Charts::assets() !!}
 @stop
 @section('content')
-  <h1> <a href="/periods/{{$period->id}}/charts">{{ $period->title }} </a> </h1>
-    <table width="100%"><th width="1%"></th><th width="23%">
-  <table style="width:50%">
-    <tr>
-      <th>Title</th>
-      <th>Balance</th>
-    </tr>
-    @foreach ($period->accounts as $account)
-      <tr>
-        <th><a href="/periods/{{$period->id}}/accounts/{{$account->id}}"> {{ $account->title }}</a> </th>
-        <th><a href="/periods/{{$period->id}}/accounts/{{$account->id}}">
-                <font color="{{$account->getColor()}}"> ${{ $account->getBalance() }} </font>
-            </a>
-        </th>
-        <th>
-          <form method="POST" action="/periods/{{$period->id}}/accounts/{{$account->id}}">
-            {{ method_field('DELETE') }}
-            <div class="form-group">
-              <button type="submit"><img id="flag" src="/img/trash.png" width="15" height="15" class="delete"></button>
+    <div class="clear-fix">
+        <div class="sideBar">
+            <div class="sideBar__section top"><h1>Accounts</h1></div>
+            <div class="sideBar__section top">
+        @foreach ($period->accounts as $account)
+            <div class="sideBar__section form-group">
+                <div class="account-container">
+                    <div class="link-container">
+                        <a href="/periods/{{$period->id}}/accounts/{{$account->id}}" class="color-neutral"> {{ $account->title }}</a>
+                        <a href="/periods/{{$period->id}}/accounts/{{$account->id}}" class="{{ $account->getColor() == 'red' ? 'color-negative' : 'color-positive' }}">${{ $account->getBalance() }}</a>
+                    </div>
+                    <div class="link-container-button">
+                        <form method="POST" action="/periods/{{$period->id}}/accounts/{{$account->id}}">
+                        {{ method_field('DELETE') }}
+                          <button type="submit" class="trash"><img id="flag" src="/img/trash.png" width="15" height="15" class="delete"></button>
+
+                        {{ csrf_field() }}
+                        </form>
+                    </div>
+                </div>
             </div>
-            {{ csrf_field() }}
-          </form>
-        </th>
-      </tr>
-    @endforeach
-  </table></th><th width="40%">{!! $chart->render() !!}</th><th width="36%"></th></table>
-  <h2>Final Total <font color="{{$period->getMercurialColor()}}">${{ $period->getMercurialAmount() }} </font></h2>
-  <h3>Add New Account</h3>
-  <form method="POST" action="/periods/{{$period->id}}/accounts">
-    <div class="form-group">
-      <table>
-          <tr><th>Title: </th><th><select name="id" autofocus title="title">
-              @foreach ($accountCategories as $accountCategory)
-                  <option value="{{ $accountCategory->id }}">{{ $accountCategory->name }}</option>
-              @endforeach
-          </select></th></tr>
-        <tr><th>Balance:$</th><th><input type="number" step="0.01" maxlength="32" value='0.00' placeholder='0.00' name="balance" class="form-control"></th></tr>
-      </table>
+        @endforeach
+            </div>
+        <div class="form-group">
+        <h2>Final Total <font color="{{$period->getMercurialColor()}}">${{ $period->getMercurialAmount() }} </font></h2>
+        <h3>Add New Account</h3>
+        <form method="POST" action="/periods/{{$period->id}}/accounts">
+
+            Title:<div class="input-field"><select name="id" autofocus title="title">
+                  @foreach ($accountCategories as $accountCategory)
+                      <option value="{{ $accountCategory->id }}">{{ $accountCategory->name }}</option>
+                  @endforeach
+                </select></div>
+            Balance:$<div class="input-field"><input type="number" step="0.01" maxlength="32" value='0.00' placeholder='0.00' name="balance"></div>
+
+        <div class="form-group">
+          <button type="submit">Add Account</button>
+        </div>
+        {{ csrf_field() }}
+        </form>
+        </div>
+        </div>
+        <div>
+            <section class="chart">
+            {!! $chart->render() !!}
+        </section>
+        </div>
     </div>
-    <div class="form-group">
-      <button type="submit">Add Account</button>
-    </div>
-    {{ csrf_field() }}
-  </form>
 @stop
